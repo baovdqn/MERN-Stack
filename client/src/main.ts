54901084178fc3484e3dfa2d654b77e79b974ac1
ha.leminh <ha.leminh@adamosoft.com>
2022-12-20 09:55:07 +0700

    change name

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Antd from 'ant-design-vue'
 
 import './assets/base-scss/main.scss'
+import 'ant-design-vue/dist/antd.css';
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router);
+
+//use Ant design
+
+app.use(Antd);
 
 app.mount('#app')
---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="dashboard-container">
+    <SideNavVue></SideNavVue>
+    <div class="dashboard-content">
+      right
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
+
+<script lang="ts">
+import SideNavVue from '@/components/icons/SideNav.vue';
+
+export default {
+  components: {
+    SideNavVue
+  },
+  setup() {
+
+  }
+}
+</script>
+
